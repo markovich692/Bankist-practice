3848f38c0b7e92920c5b849a2ebefb29fd96f5bf
Marc Diallo <mark.devguru@outlook.com>
2025-02-09 20:10:49 -0600

    inserts the movements using the insertAdjacentHTML methods

--- script.js
+++ script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+containerApp.style.opacity = 1;
+
+const html = `<div class="movements">
+        <div class="movements__row">
+          <div class="movements__type movements__type--deposit">2 deposit</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">4 000€</div>
+        </div>
+        <div class="movements__row">
+          <div class="movements__type movements__type--withdrawal">
+            1 withdrawal
+          </div>
+          <div class="movements__date">24/01/2037</div>
+          <div class="movements__value">-378€</div>
+        </div>
+      </div>`;
+
+containerApp.insertAdjacentHTML('afterbegin', html);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,4 +93,3 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-containerApp.style.opacity = 1;
